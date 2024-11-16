87ac88a6e56001d21988031a32c34f0846ea0062
Vaibhav Darvekar <vabdarwekar00@gmail.com>
2024-11-16 22:09:06 +0530

    Update app component

--- climate/src/App.tsx
+++ climate/src/App.tsx
@@ -1,11 +1,11 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./App.css";
-import { ThemeProvider } from "./context/theme-provider";
-import WeatherDashboard from "./pages/weather-dashboard";
-import CityPage from "./pages/city-page";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "./components/ui/sonner";
+import { ThemeProvider } from "./context/theme-provider";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./components/layout";
+import WeatherDashboard from "./pages/weather-dashboard";
+import { CityPage } from "./pages/city-page";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -17,6 +17,7 @@ const queryClient = new QueryClient({
     },
   },
 });
+
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
@@ -28,6 +29,7 @@ function App() {
               <Route path="/city/:cityName" element={<CityPage />} />
             </Routes>
           </Layout>
+          <Toaster richColors />
         </ThemeProvider>
       </BrowserRouter>
       <ReactQueryDevtools initialIsOpen={false} />
