ee67ea22288c08e7f83b2a43c5c38e3ad8a82528
Vaibhav Darvekar <vabdarwekar00@gmail.com>
2024-11-16 22:10:04 +0530

    refactor header component and seperate theme and city serch component

--- climate/src/components/header.tsx
+++ climate/src/components/header.tsx
@@ -1,35 +1,27 @@
-import { useTheme } from "@/context/theme-provider";
-import { Moon, Sun } from "lucide-react";
 import { Link } from "react-router-dom";
+import { CitySearch } from "./city-search";
+import { useTheme } from "@/context/theme-provider";
+import { ThemeToggle } from "./theme-toggler";
 
-const Header = () => {
-  const { theme, setTheme } = useTheme();
-  const isDark = theme === "dark";
+export function Header() {
+  const { theme } = useTheme();
 
   return (
-    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur py-2 supports-[backdrop-filter]:bg-background/60">
-      <div className="container mx-auto flex h-16 items-container justify-between px-4">
+    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 py-2">
+      <div className="container mx-auto flex h-16 items-center justify-between px-4">
         <Link to={"/"}>
           <img
-            src={isDark ? "/logo.png" : "/logo2.png"}
-            alt="Klimate Logo"
+            src={theme === "dark" ? "/logo.png" : "/logo2.png"}
+            alt="Klimate logo"
             className="h-14"
           />
         </Link>
-        <div
-          onClick={() => setTheme(isDark ? "light" : "dark")}
-          className={`flex items-center cursor-pointer transition-transform duration-500
-                ${isDark ? "rotate-180" : "rotate-0"}`}
-        >
-          {isDark ? (
-            <Sun className="h-6 w-6 text-yellow-500 rotate-0 translate-all" />
-          ) : (
-            <Moon className="h-6 w-6 text-blue-500 rotate-0 translate-all" />
-          )}
+
+        <div className="flex gap-4">
+          <CitySearch />
+          <ThemeToggle />
         </div>
       </div>
     </header>
   );
-};
-
-export default Header;
+}
